e247c9eb007c1c805e0b98108278afab99f762e0
Brian Gitau <77824027+vicenet@users.noreply.github.com>
2023-12-06 12:20:38 +0300

    Update forms.py

--- intergration/forms.py
+++ intergration/forms.py
@@ -27,20 +27,8 @@ class AccountBalanceForm(forms.Form):
     pass
 
 class ReversalForm(forms.Form):
-<<<<<<< HEAD
-<<<<<<< HEAD
-    transaction_id = forms.CharField()
-    amount = forms.IntegerField()
-    remarks = forms.CharField()
-    occasion = forms.CharField()
-=======
-=======
->>>>>>> origin/alter
     transaction_id = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     amount = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     remarks = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     occasion = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-<<<<<<< HEAD
->>>>>>> 3f0fe0a (db)
-=======
->>>>>>> origin/alter
+
